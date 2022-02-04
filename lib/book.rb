4b6f6774f6d98d3829b16f772879e1065a9092a6
Jeni Dang <jenipdang@yahoo.com>
2022-02-03 21:20:22 -0800

    commit tree a1a84d8857f3890c74d6ef7ac0fb764990d28801

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :genre, :page_count
+
+
+    def initialize(title = "And Then There Were None")
+        @title = title 
+    end
+
+    #using attribute accessor (set and get) don't need to write additional getter
+    # def author
+    #     @author
+    # end
+
+    # def page_count
+    #     @page_count
+    # end
+
+    # def genre
+    #     @genre
+    # end
+    #------------------end of comment ------------#
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,32 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand 
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand = "Adidas")
+        @brand = brand
+    end 
+
+    # ------use attribute accessor, dont need to write additional getter method --#
+    # def color
+    #     @color
+    # end
+
+    # def size
+    #     @size
+    # end
+
+    # def material 
+    #     @material
+    # end
+
+    # def condition
+    #     @condition
+    # end
+    #--------end of comment ------#
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
